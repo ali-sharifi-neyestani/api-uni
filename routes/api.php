86a44a026d0b1c2b6d808adfc93f9869f8b1d96d
Ali-Sharifi-Neyestani <hacka.kootlas@gmail.com>
2020-04-13 17:07:34 +0430

    add dev tools

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
+
+
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Dev tools
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'namespace'  => 'Dev',
+    'prefix'     => 'v1/dev',
+], function ($router) {
+    require base_path('routes/dev.php');
 });
+
