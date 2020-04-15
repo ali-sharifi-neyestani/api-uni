e1c44069790ef40eb9b52a928cd8b6508f5e1ca7
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-04-15 20:03:29 +0430

    change users Table

---
+++ database/migrations/2020_04_15_140623_change_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropUnique(['email']);
+            $table->string('email')->nullable()->unique()->change();
+            $table->string('national_code')->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropUnique(['email']);
+            $table->string('email')->unique()->change();
+            $table->dropColumn('national_code');
+        });
+    }
+}
